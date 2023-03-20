7b98b298d5c3d41087cb0b36d3011b278aa72aea
KulNros <kulnros@gmail.com>
2023-03-20 11:57:20 +0700

    chenge font

--- UI/UI.java
+++ UI/UI.java
@@ -12,16 +12,16 @@ import Main.GamePanel;
 
 public class UI {
     GamePanel GP;
-    Font arial;
-    Font bebause;
+    Font TAGAMEBOY_1;
+    Font TAGAMEBOY_2;
     BufferedImage Pokeball;
     
     GamePanel position;
 
     public UI(GamePanel GP){
         this.GP=GP;
-        arial = new Font("Arial",Font.BOLD,25);
-        bebause = new Font("Bebas Neue",Font.BOLD,25);
+        TAGAMEBOY_1 = new Font("TA Gameboy",Font.BOLD,30);
+        TAGAMEBOY_2 = new Font("TA Gameboy",Font.BOLD,25);
         try {
             Pokeball = ImageIO.read(getClass().getResourceAsStream("/UI/Pokeball.png"));
             
@@ -33,9 +33,10 @@ public class UI {
     public void endgame(Graphics2D g2d){
         g2d.setColor(new Color(0,0,0));
         g2d.fillRoundRect(0, 0, 1000, 1000, 0,0);
-        g2d.setFont(arial);
+        g2d.setFont(TAGAMEBOY_1);
         g2d.setColor(Color.white);
-        g2d.drawString("END GAME CONGRATULATION YOU CAN LEAVE NOW HIT THE X BUTTON :)",25,position.screenHeight/2);
+        g2d.drawString("END GAME CONGRATULATION!! ",155,position.screenHeight/2);
+        g2d.drawString("YOU CAN LEAVE NOW HIT THE X BUTTON :)",45,position.screenHeight/2+50);
         
         
     }
@@ -44,16 +45,16 @@ public class UI {
         g2d.setColor(new Color(0,0,0,200));
         g2d.fillRoundRect(position.screenWidth/2-200, 650, 400, 100, 35,35);
         
-        g2d.setFont(bebause);
+        g2d.setFont(TAGAMEBOY_2);
         g2d.setColor(Color.black);
-        g2d.drawString("FIND THE POKEMON",position.screenWidth/2-120,710);
+        g2d.drawString("FIND THE POKEMON",position.screenWidth/2-160,710);
         g2d.setColor(Color.white);
-        g2d.drawString("FIND THE POKEMON",position.screenWidth/2-124,710);
+        g2d.drawString("FIND THE POKEMON",position.screenWidth/2-164,710);
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
         
         g2d.drawImage(Pokeball, GP.titleSize/2, GP.titleSize/2, GP.titleSize,GP.titleSize,null);
 
-        g2d.setFont(bebause);
+        g2d.setFont(TAGAMEBOY_2);
         g2d.setColor(Color.black);
         g2d.drawString(" X "+GP.PokemonCount,74,52);
         g2d.setColor(Color.white);
